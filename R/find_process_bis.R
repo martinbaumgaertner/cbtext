59955f89cef77bb96ee05144fa4f59c4d0039912
martinbaumgaertner <martin.baumgaertner1@gmail.com>
2021-08-26 16:29:43 +0200

    cleanup

--- R/find_process_bis.R
+++ R/find_process_bis.R
@@ -1,7 +1,7 @@
 find_process_bis<-function(max_page){
   countries_cb<-get_bis_countries()
   
-  d<-future::future(lapply(as.list(1:max_page),read_bis_page,countries_cb))
-   return(d%>%
-     dplyr::bind_rows())
+  d<-future::future(lapply(as.list(1:max_page),read_bis_page,countries_cb))%>%
+    dplyr::bind_rows()
+  return(d)
 }
