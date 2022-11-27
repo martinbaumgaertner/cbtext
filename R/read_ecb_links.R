de15e767357b3b010338029f77a654bb7222a35a
martinbaumgaertner <martin.baumgaertner1@gmail.com>
2022-11-27 18:51:39 +0100

    up

--- R/read_ecb_links.R
+++ R/read_ecb_links.R
@@ -37,3 +37,27 @@ read_ecb_pc_links<-function(x){
   part<-unique(part[grepl(".en.",part)])
   paste0("https://www.ecb.europa.eu",part)
 }
+
+x=2019
+read_ecb_interviw_links<-function(x){
+  part<-xml2::read_html(paste0("https://www.ecb.europa.eu/press/key/date/",x,"/html/index_include.en.html"))%>%
+    rvest::html_nodes("a")%>%
+    rvest::html_attr("href")
+  part<-unique(part[grepl(".en.",part)])
+  paste0("https://www.ecb.europa.eu",part)
+}
+
+xml2::read_html(paste0("https://www.ecb.europa.eu/press/inter/date/",x,"/html/index_include.en.html")) %>%
+  rvest::html_nodes(".title")%>%
+  rvest::html_nodes("a")%>%
+  rvest::html_attr("href")
+
+
+xml2::read_html(paste0("https://www.ecb.europa.eu/press/inter/date/",x,"/html/index_include.en.html")) %>%
+  rvest::html_nodes(".date")%>% 
+  html_text()
+
+xml2::read_html(paste0("https://www.ecb.europa.eu/press/inter/date/",x,"/html/index_include.en.html")) %>%
+  rvest::html_nodes(".title")%>% 
+  html_text() %>% 
+  head(unlist(str_split(.,":",simplify = T)))
