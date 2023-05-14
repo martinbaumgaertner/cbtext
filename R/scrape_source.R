ff319763b301602da5f34f628a95fe059477bb44
martinbaumgaertner <martin.baumgaertner1@gmail.com>
2023-05-14 16:50:14 +0200

    updated doku

--- R/scrape_source.R
+++ R/scrape_source.R
@@ -12,7 +12,7 @@
 #'   x<-scrape_source(list(name="fed"),c("minutes","pc","red","transkript","agenda",
 #'   "blue","green_sub","teala","tealb","green1","green2","beige"))
 #'   x<-scrape_source(list(name="boj"),c("minutes","economic_report","release","outlook_report"))
-#'   x<-scrape_source(list(name="poland"),c("minutes"))
+#'   x<-scrape_source(list(name="poland"),c("minutes","inflation_report","release"))
 #'   x<-scrape_source(list(name="iceland"),c("minutes"))
 #'   x<-scrape_source(list(name="australia"),c("minutes"))
 #'   x<-scrape_source(list(name="riksbank"),c("minutes","economicreview","mpreports"))
@@ -22,7 +22,8 @@
 #' @export 
 scrape_source<-function(source,parts){
   output<-list()
-  Sys.setlocale("LC_ALL","English")
+  Sys.setlocale("LC_ALL","en_US.UTF-8")
+  
   for(part in parts){
     message(paste("Find",part))
     if(part=="speeches"){
@@ -34,5 +35,7 @@ scrape_source<-function(source,parts){
       output[[part]]<-load_data_from_cb(links,part,source$name)
     }
   }
+  
   return(output)
 }
+
