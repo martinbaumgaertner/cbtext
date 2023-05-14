b3477c33f39102b53f4581a0e787436deb49a7cf
martinbaumgaertner <martin.baumgaertner1@gmail.com>
2023-05-14 17:26:32 +0200

    not sure

--- R/read_text.R
+++ R/read_text.R
@@ -27,30 +27,26 @@ read_text<-function(x,sour){
   }
 
 clean_html<-function(x){
-  x1 <- ifelse(stringr::str_detect(x, "#"),
-               suppressMessages(xml2::read_html(x)),
-               suppressMessages(xml2::read_html(url(x))))
-  
+  if(stringr::str_detect(x,"#")){
+    x1<-suppressMessages(xml2::read_html(x))
+  }else{
+    x1<-suppressMessages(xml2::read_html(url(x)))
+  }
   #fedheading
   heading<-x1%>% rvest::html_nodes(".jumbotron.hidden-xs")
   xml2::xml_remove(heading)
-  
   #fed sidebar
   navbar<-x1 %>% rvest::html_nodes(".nav__header")
   xml2::xml_remove(navbar)
-  
   #ecb footer
   footer_ecb<-x1 %>% rvest::html_nodes(".ecb-footerBottom")
   xml2::xml_remove(footer_ecb)
-  
   adress_ecb<-x1 %>% rvest::html_nodes(".address-box.-top-arrow")
   xml2::xml_remove(adress_ecb)
-  
   cookies_ecb<-x1 %>% rvest::html_nodes(".ecb-cookieConsent.hidden")
   xml2::xml_remove(cookies_ecb)
   
   out<-x1
+  
   return(out)
 }
-
-
